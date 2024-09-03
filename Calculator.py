57fbfcd38cd1d23bb7e79c6cf9f6febfe95a89be
Pulkit1903 <pulkit.19.2003@gmail.com>
2024-09-03 16:03:49 +0530

    Calcualtor.py Added?

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+print("\tWelcome to my calculator")
+
+num1 = int(input("Enter a number: "))  #Type casting str->int
+num2 = int(input("Enter a number: "))
+
+print("Addition:",num1 + num2)
+print("Difference:",num1 - num2)
+print("Multiplication:",num1 * num2)
+
+print(type(num1))
+print(type(num2))
