bce9e5fe21a209a1048a82008fb68910bfcafd84
Pulkit1903 <pulkit.19.2003@gmail.com>
2024-09-05 18:17:15 +0530

    Util.py updated using User Function

--- Utils.py
+++ Utils.py
@@ -1,3 +1,13 @@
 #Import-> To use/import python libraries
 import os
-print(os.system('systeminfo'))
+import datetime
+
+#To Print System Info using User Function
+def sys_info(command):
+    print(os.system(command))
+sys_info("systeminfo")
+
+#To Print Date of Today using User Function
+def date_today():
+    return datetime.datetime.today()
+print(date_today())
